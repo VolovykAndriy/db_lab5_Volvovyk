7567dc1b616a1cc78fb7cc9fd121b169b353f29e
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-19 20:04:51 +0200

    Update create.sql

--- create.sql
+++ create.sql
@@ -12,14 +12,6 @@ CREATE TABLE Publisher
   PRIMARY KEY (publisher_id)
 );
 
-CREATE TABLE global_sales
-(
-  gs_id INT NOT NULL,
-  sales FLOAT NOT NULL,
-  year_of_update INT NOT NULL,
-  PRIMARY KEY (gs_id)
-);
-
 CREATE TABLE Game
 (
   game_id INT NOT NULL,
@@ -28,11 +20,19 @@ CREATE TABLE Game
   Genre VARCHAR(50) NOT NULL,
   platform_id INT NOT NULL,
   publisher_id INT NOT NULL,
-  gs_id INT NOT NULL,  
   PRIMARY KEY (game_id),
   FOREIGN KEY (platform_id) REFERENCES Platform(platform_id),
-  FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id),
-  FOREIGN KEY (gs_id) REFERENCES global_sales(gs_id)  
+  FOREIGN KEY (publisher_id) REFERENCES Publisher(publisher_id), 
+);
+
+CREATE TABLE global_sales
+(
+  gs_id INT NOT NULL,
+  game_id INT NOT NULL,
+  sales FLOAT NOT NULL,
+  year_of_update INT NOT NULL,
+  PRIMARY KEY (gs_id),
+  FOREIGN KEY (game_id) REFERENCES game(game_id)  
 );
 
 CREATE TABLE Platform_has_game
