2f889d3e930ffa7df14d0be2172875daac26a83e
VolovykAndriy <91112412+VolovykAndriy@users.noreply.github.com>
2023-12-19 20:05:54 +0200

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -2,8 +2,8 @@ DO $$
 BEGIN
 	FOR i in 1..30
 		LOOP 
-			INSERT INTO global_sales (gs_id, sales, year_of_update)
-			VALUES (i, i * 1000000, 2020);
+			INSERT INTO global_sales (gs_id, game_id, sales, year_of_update)
+			VALUES (i, i*10, i * 1000000, 2020);
 		END LOOP;
 END;
 $$
